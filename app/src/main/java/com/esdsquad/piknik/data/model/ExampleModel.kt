a689c2a55f48123b4d72fa4f4553bfdbc0e709fe
nando <muhammad.rezki.ananda@gmail.com>
2021-07-04 22:30:42 +0700

    Add article

--- app/src/main/java/com/esdsquad/piknik/data/model/ExampleModel.kt
+++
@@ -1,7 +0,0 @@
-package com.esdsquad.piknik.data.model
-
-data class ExampleModel(
-    val exampleString: String,
-    val exampleInt: Int,
-    val exampleBoolean: Boolean
-)
--- app/src/main/java/com/esdsquad/piknik/data/view/fragment/bottomnav/BerandaFragment.kt
+++ app/src/main/java/com/esdsquad/piknik/data/view/fragment/bottomnav/BerandaFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.esdsquad.piknik.R
+import com.esdsquad.piknik.data.model.ArticelModel
+import com.esdsquad.piknik.data.view.adapter.ArticelAdapter
 import com.esdsquad.piknik.data.view.adapter.ImageSliderAdapter
 import com.esdsquad.piknik.databinding.FragmentBerandaBinding
 
@@ -21,6 +23,8 @@ class BerandaFragment : Fragment() {
         R.drawable.iklan_2
     )
 
+    private lateinit var adapter: ArticelAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,9 +36,37 @@ class BerandaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupView()
+        setupRecycerView()
         setupViewModel()
         setupListener()
         setupObserver()
+    }
+
+    private fun setupRecycerView() {
+        val listAdapter: ArrayList<ArticelModel> = ArrayList()
+        listAdapter.add(
+            ArticelModel(
+                1,
+                "PPKM Tahap Dua Kota Bandung Dimulai, Ini Daftar Perubahan Aturannya",
+                "https://pict.sindonews.net/dyn/620/pena/news/2021/01/27/701/315478/ppkm-tahap-dua-kota-bandung-dimulai-ini-daftar-perubahan-aturannya-kmx.png"
+            )
+        )
+        listAdapter.add(
+            ArticelModel(
+                2,
+                "Aturan Lengkap PPKM Darurat di Jawa-Bali Selama 3-20 Juli",
+                "https://akcdn.detik.net.id/visual/2021/06/23/jokowi_169.jpeg"
+            )
+        )
+        listAdapter.add(
+            ArticelModel(
+                3,
+                "Mengenal apa itu Staycation, Kamu harus coba konsep ini",
+                "https://ik.imagekit.io/tvlk/blog/2020/01/Staycation-1-Pixabay.jpg"
+            )
+        )
+        adapter = ArticelAdapter(listAdapter)
+        binding.listArtikel.adapter = adapter
     }
 
     private fun setupView() {
